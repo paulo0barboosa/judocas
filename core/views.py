b951bba471208b2412591ebda485731a58623dc5
paulo0barboosa <paulohwbarbosa@gmail.com>
2020-11-04 18:56:03 -0300

    subindo funcionalidade de delete

--- core/views.py
+++ core/views.py
@@ -5,70 +5,39 @@ from django.views.generic import TemplateView, ListView
 from django.db.models import Q
 from .models import Filiado, Professor, Academia, Pessoa
 from .forms import FiliadoForm, ProfessorForm, AcademiaForm
-# Create your views here.
+
 
 def home(request):
     return render(request, 'core/index.html')
 
-@login_required
-def filiados_cadastrados(request):
-    filiados = Filiado.objects.all()
-    return render(request, 'core/filiados_cadastrados.html', {'filiados':filiados})
 
-# @login_required
-# def cadastra_filiado(request):
-#     # form_struct = FiliadoForm()
-#     form = FiliadoForm(request.POST or None)
-#     if form.is_valid():
-#         form.save()
-#     return render(request, 'core/cadastra_filiado.html', {'form' : form})
-
+# -------------- CADASTRO --------------
 @login_required
 def cadastra_filiado(request):
-    # academia = get_object_or_404(Academia)
-    # form_struct = FiliadoForm()
     form = FiliadoForm(request.POST or None)
     if form.is_valid():
         form.save()
+        return redirect("core_cadastra_filiado")
     return render(request, 'core/cadastra_filiado.html', {'form' : form})
 
 @login_required
-def update_pessoa(request, RegistroCBJ):
-    data = {}
-    # pessoa = Pessoa.objects.get(RegistroCBJ=RegistroCBJ)
-
-    try:
-        pessoa = Filiado.objects.get(RegistroCBJ=RegistroCBJ)
-        form = FiliadoForm(request.POST or None, instance=pessoa)
-    except Filiado.DoesNotExist:
-        pessoa = Professor.objects.get(RegistroCBJ=RegistroCBJ)   
-        form = ProfessorForm(request.POST or None, instance=pessoa)
-
-    # professor = Professor.objects.get(RegistroCBJ=RegistroCBJ)
-    # filiado = Filiado.objects.get(RegistroCBJ=RegistroCBJ)
-
-    # if professor != 
-
-    # form = PessoaForm(request.POST or None, instance=pessoa)
-    data['pessoa'] = pessoa
-    data['form'] = form
-
-    if request.method == 'POST':
-        if form.is_valid():
-            form.save()
-            return redirect("search_pessoa")
-    # else:
-    return render(request, 'core/update_pessoa.html', data)
-
-@login_required
 def cadastra_professor(request):
-    # form_struct = FiliadoForm()
     form = ProfessorForm(request.POST or None)
     if form.is_valid():
         form.save()
+        return redirect("core_cadastra_professor")
     return render(request, 'core/cadastra_professor.html', {'form' : form})
 
-# @login_required
+@login_required
+def cadastra_academia(request):
+    form = AcademiaForm(request.POST or None)
+    if form.is_valid():
+        form.save()
+        return redirect("core_cadastra_academia")
+    return render(request, 'core/cadastra_academia.html', {'form' : form})
+
+
+# -------------- BUSCA --------------
 class search_pessoa(ListView):
     model = Pessoa
     template_name = 'core/search_pessoa.html'
@@ -95,23 +64,28 @@ class search_academia(ListView):
         )
         return object_list
 
-@login_required
-def professores_cadastrados(request):
-    professores = Professor.objects.all()
-    return render(request, 'core/professores_cadastrados.html', {'professores':professores})
 
+# -------------- UPDATE --------------
 @login_required
-def cadastra_academia(request):
-    # form_struct = FiliadoForm()
-    form = AcademiaForm(request.POST or None)
-    if form.is_valid():
-        form.save()
-    return render(request, 'core/cadastra_academia.html', {'form' : form})
+def update_pessoa(request, RegistroCBJ):
+    data = {}
 
-@login_required
-def academias_cadastrados(request):
-    academias = Academia.objects.all()
-    return render(request, 'core/academias_cadastrados.html', {'academias':academias})
+    try:
+        pessoa = Filiado.objects.get(RegistroCBJ=RegistroCBJ)
+        form = FiliadoForm(request.POST or None, instance=pessoa)
+    except Filiado.DoesNotExist:
+        pessoa = Professor.objects.get(RegistroCBJ=RegistroCBJ)   
+        form = ProfessorForm(request.POST or None, instance=pessoa)
+
+    data['pessoa'] = pessoa
+    data['form'] = form
+
+    if request.method == 'POST':
+        if form.is_valid():
+            form.save()
+            return redirect("search_pessoa")
+    # else:
+    return render(request, 'core/update_pessoa.html', data)
 
 @login_required
 def update_academia(request, IDAcademia):
@@ -127,3 +101,46 @@ def update_academia(request, IDAcademia):
             return redirect("search_academia")
     # else:
     return render(request, 'core/update_academia.html', data)
+
+
+# -------------- DELETE --------------
+@login_required
+def delete_pessoa(request, RegistroCBJ):
+    try:
+        pessoa = Filiado.objects.get(RegistroCBJ=RegistroCBJ)
+    except Filiado.DoesNotExist:
+        pessoa = Professor.objects.get(RegistroCBJ=RegistroCBJ)   
+
+    if request.method == 'POST':
+        pessoa.delete()
+        return redirect("search_pessoa")
+    # else:
+    return render(request, 'core/delete_pessoa.html', {'pessoa': pessoa})
+
+@login_required
+def delete_academia(request, IDAcademia):
+    academia = Academia.objects.get(IDAcademia=IDAcademia)
+
+    if request.method == 'POST':
+        academia.delete()
+        return redirect("search_academia")
+    # else:
+    return render(request, 'core/delete_academia.html', {'academia': academia})
+
+
+# -------------- LISTAGEM --------------
+
+# @login_required
+# def filiados_cadastrados(request):
+#     filiados = Filiado.objects.all()
+#     return render(request, 'core/filiados_cadastrados.html', {'filiados':filiados})
+
+# @login_required
+# def professores_cadastrados(request):
+#     professores = Professor.objects.all()
+#     return render(request, 'core/professores_cadastrados.html', {'professores':professores})
+
+# @login_required
+# def academias_cadastrados(request):
+#     academias = Academia.objects.all()
+#     return render(request, 'core/academias_cadastrados.html', {'academias':academias})
